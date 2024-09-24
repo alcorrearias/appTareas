6d3385dde12c57257948464ecf9d817c4d021c00
Ana <135356525+alcorrearias@users.noreply.github.com>
2024-09-24 11:17:56 -0500

    creando el proyecto inicial

--- apptareas/lib/main.dart
+++ apptareas/lib/main.dart
@@ -1,27 +1,49 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: HomePage()
     ),
   );
 }
 
+class MyModel {
+  final String titulo;
+  final String estado;
+
+  MyModel({required this.titulo, required this.estado});
+
+  factory MyModel.fromJson(Map<String, dynamic> json) {
+    return MyModel(
+      titulo: json['titulo'],
+      estado: json['estado'],
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
- 
+  const HomePage({super.key}); 
 
   // This widget is the root of your application.
   @override
-  _HomePageState createState() =>_HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  
+    
   getTareas() async {
-    http.Response response = await http.get(Uri.http("http://127.0.0.1:8001/api/tareas"));
+    var url = Uri.parse('http://127.0.0.1:8001/api/tareas');
+    http.Response response = await http.get(url);    
     debugPrint(response.body);
+    if (response.statusCode == 200) {
+      
+  } else {
+    
+  }
+    
   }
   
 @override
@@ -30,8 +52,9 @@ class _HomePageState extends State<HomePage> {
     getTareas();
   }
 
+  @override
   Widget build(BuildContext context) {
-      return Scaffold(appBar: AppBar(title: Text('Lista Tareas') ,
+      return Scaffold(appBar: AppBar(title: const Text('Lista Tareas') ,
       backgroundColor: Colors.indigo[300],),
       //body: ListView.builder(),
       );
